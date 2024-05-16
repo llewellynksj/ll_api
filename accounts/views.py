1c43750810c319a85f6abff098a55ae015e341a6
Kate L <llewellyn.ksj@gmail.com>
2024-05-16 15:20:54 +0100

    Refactor views in accounts and leaf

--- accounts/views.py
+++ accounts/views.py
@@ -1,39 +1,20 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Account
 from .serializers import AccountSerializer
 from ll_api.permissions import IsOwnerOrReadOnly
 
-class AccountList(APIView):
-    def get(self, request):
-        accounts = Account.objects.all()
-        serializer = AccountSerializer(accounts, many=True, context={'request': request})
-        return Response(serializer.data)
+class AccountList(generics.ListAPIView):
+    """
+    Lists all accounts belonging to registered users
+    """
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
     
 
-class AccountDetail(APIView):
-    serializer_class = AccountSerializer
+class AccountDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve and update account details
+    """
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            account = Account.objects.get(pk=pk)
-            self.check_object_permissions(self.request, account)
-            return account
-        except Account.DoesNotExist:
-            raise Http404
-        
-    def get(self, request, pk):
-        account = self.get_object(pk)
-        serializer = AccountSerializer(account, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        account = self.get_object(pk)
-        serializer = AccountSerializer(account, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
