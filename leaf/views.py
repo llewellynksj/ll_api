018b1ca40bf8e1a2b7e77793961f9624ecd0efe4
Kate L <llewellyn.ksj@gmail.com>
2024-05-17 09:26:43 +0100

    Add search function to leaf views

--- leaf/views.py
+++ leaf/views.py
@@ -16,8 +16,9 @@ class LeafList(generics.ListCreateAPIView):
   permission_classes = [
     permissions.IsAuthenticatedOrReadOnly
   ]
-  filter_backends = [filters.OrderingFilter]
+  filter_backends = [filters.OrderingFilter, filters.SearchFilter]
   ordering_fields = ['created_at', 'remember_count']
+  search_fields = ['name', 'memory', 'user', 'due_date']
   
   def perform_create(self, serializer):
     serializer.save(owner=self.request.user)
