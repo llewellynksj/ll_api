bc2c6aa0bd7f3e9ed11da7a50d98e04d5871ab57
Kate L <llewellyn.ksj@gmail.com>
2024-06-04 09:33:31 +0100

    Add JWT AUTH HTTPONLY (false) to settings to attempt fix

--- ll_api/settings.py
+++ ll_api/settings.py
@@ -85,11 +85,16 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-REST_USE_JWT = True
-JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_HTTPONLY': False,
+    'REST_USE_JWT': True,
+    'JWT_AUTH_SECURE': True,
+    'JWT_AUTH_COOKIE': 'my-app-auth',
+    'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
+    'JWT_AUTH_SAMESITE': 'None',
+} 
+
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'll_api.serializers.CurrentUserSerializer',
