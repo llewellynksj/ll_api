463573a5fbf6868c2b5ac4aefab79d9ab2998f3e
Kate L <llewellyn.ksj@gmail.com>
2024-06-04 08:09:34 +0100

    Amend DEBUG in env to attempt bug fix

--- ll_api/settings.py
+++ ll_api/settings.py
@@ -85,15 +85,10 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('Bearer',),
-}
-
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'll_api.serializers.CurrentUserSerializer',
